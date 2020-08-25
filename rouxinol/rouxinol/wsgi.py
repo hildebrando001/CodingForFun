da72f8de1f19341046b5f09e04b03a82219f70a7
José Hildebrando <jose.hildebrando@gmail.com>
2020-08-25 19:34:06 -0300

    model

---
+++ rouxinol/rouxinol/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rouxinol project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rouxinol.settings')
+
+application = get_wsgi_application()
